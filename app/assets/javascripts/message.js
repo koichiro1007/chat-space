0c6c9ce52899d14424608a0ef9557bf48b498255
koichiro1007 <koichiro1007@gmail.com>
2020-03-01 12:14:58 +0900

    ChatSpace メッセージ送信の非同期化

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,65 @@
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="chat__list__message">
+         <div class="chat__list__message__info">
+           <div class="chat__list__message__info__name">
+             ${message.user_name}
+           </div>
+           <div class="chat__list__message__info__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="chat__list__message__text">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="chat__list__message">
+         <div class="chat__list__message__info">
+           <div class="chat__list__message__info__name">
+             ${message.user_name}
+           </div>
+           <div class="chat__list__message__info__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="chat__list__message__text">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+   };
+ }
+$('#new_message').on('submit', function(e){
+ e.preventDefault();
+ var formData = new FormData(this);
+ var url = $(this).attr('action')
+ $.ajax({
+   url: url,
+   type: "POST",
+   data: formData,
+   dataType: 'json',
+   processData: false,
+   contentType: false
+ })
+ .done(function(data){
+  var html = buildHTML(data);
+  $('.chat__list').append(html);
+  $('.chat__list').animate({ scrollTop: $('.chat__list')[0].scrollHeight});
+  $('form')[0].reset();
+  $(".chat__form__send").prop("disabled", false);
+})
+ .fail(function() {
+   alert("メッセージに失敗しました");
+ })
+})
+});
